1fc86b1fa6464d78358a260fc52bd451b9990158
Vini_Diaz <133820166+ViniDiazTec@users.noreply.github.com>
2024-09-21 22:25:26 -0300

    inicialização do esqueleto protótipo com integração de data base sql JPA maven

---
+++ src/main/java/com/cinema/db/integradatabase/IntegradatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.cinema.db.integradatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegradatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegradatabaseApplication.class, args);
+	}
+
+}
